ba42cf48ad6be88d03e737ca6a690da4f7aa9eae
zhouzheng <zznewstart891222@gmail.com>
2024-05-31 18:40:13 +0800

    save

--- app/src/main/java/com/example/myapplication/MyHook.java
+++ app/src/main/java/com/example/myapplication/MyHook.java
@@ -8,20 +8,14 @@ import me.ele.lancet.base.annotations.TargetClass;
 
 public class MyHook {
 
-    /**
-     *         initGlide();
-     *         initRetrofit();
-     *         initSDK1();
-     *         initSDK2();
-     */
     @Insert("initGlide")
     @TargetClass(value = "com.example.myapplication.LearningApp")
     public void fd() {
         long now = System.currentTimeMillis();
-        Log.d("zhouzheng  initGlide start", ""+now);
+        Log.d("zhouzheng", "initGlide start"+now);
         try {
             Origin.callVoid();
-            Log.d("zhouzheng initGlide  end", "" + (System.currentTimeMillis() - now));
+            Log.d("zhouzheng", "initGlide  end" + (System.currentTimeMillis() - now));
         }catch (Exception e) {
 
         }
@@ -31,10 +25,10 @@ public class MyHook {
     @TargetClass(value = "com.example.myapplication.LearningApp")
     public void ref() {
         long now = System.currentTimeMillis();
-        Log.d("zhouzheng  initGlide start", ""+now);
+        Log.d("zhouzheng", "retrofit start"+now);
         try {
             Origin.callVoid();
-            Log.d("zhouzheng initGlide  end", "" + (System.currentTimeMillis() - now));
+            Log.d("zhouzheng", "retrofit end" + (System.currentTimeMillis() - now));
         }catch (Exception e) {
 
         }
@@ -44,10 +38,10 @@ public class MyHook {
     @TargetClass(value = "com.example.myapplication.LearningApp")
     public void iniSDK1() {
         long now = System.currentTimeMillis();
-        Log.d("zhouzheng  initGlide start", ""+now);
+        Log.d("zhouzheng", "sdk2 start"+now);
         try {
             Origin.callVoid();
-            Log.d("zhouzheng initGlide  end", "" + (System.currentTimeMillis() - now));
+            Log.d("zhouzheng", "sdk2  end" + (System.currentTimeMillis() - now));
         }catch (Exception e) {
 
         }
@@ -57,10 +51,10 @@ public class MyHook {
     @TargetClass(value = "com.example.myapplication.LearningApp")
     public void iniSDK2() {
         long now = System.currentTimeMillis();
-        Log.d("zhouzheng  initretr start", ""+now);
+        Log.d("zhouzheng", "sdk1 start"+now);
         try {
             Origin.callVoid();
-            Log.d("zhouzheng initres  end", "" + (System.currentTimeMillis() - now));
+            Log.d("zhouzheng", "sdk1  end" + (System.currentTimeMillis() - now));
         }catch (Exception e) {
 
         }
